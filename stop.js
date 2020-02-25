4f33728482296aa223b0bfb445ca72b4f33fa29d
winicmic000 <winicmic000@warren.k12.in.us>
2020-02-25 11:00:36 -0500

    dsa

--- stop.js
+++ stop.js
@@ -1,89 +1,51 @@
-let bruv = "off";
-let theWrongNumber = document.getElementById("isItwrong");
-let count=0;
+let count = 0;
+let numRight = 0;
+let correctOne;
+let numQuestions = 4;
 
 
-let createQuestion = function(questionText, choice1, choice2, choice3, choice4, correctChoice, wrongChoice1, wrongChoice2, wrongChoice3){
+btn1 = document.getElementById("btn0");
+btn2 = document.getElementById("btn1");
+btn3 = document.getElementById("btn2");
+btn4 = document.getElementById("btn3");
 
-    choice1 = document.getElementById("btn0");
-    choice2 = document.getElementById("btn1");
-    choice3 = document.getElementById("btn2");
-    choice4 = document.getElementById("btn3");
 
+let createQuestion = function(questionText, choice1, choice2, choice3, choice4, correctChoice){
 
   questionText = document.getElementById("question");
 
-    let correctClick = function(){
-        correctChoice.onclick = count++;
-    };
-
-    let badClick1 = function(){
-        wrongChoice1.onclick = theWrongNumber.innerText = "That is wrong!";
-    };
-
-    let badClick2 = function(){
-        wrongChoice2.onclick = theWrongNumber.innerText = "That is wrong!";
-    };
-
-    let badClick3 = function(){
-        wrongChoice3.onclick = theWrongNumber.innerText = "That is wrong!";
-    };
-
-    if(count === 0){
-
-        questionText.innerText = "What is the square root of 4?";
-        choice1.innerText = "Wrong";
-        choice2.innerText = "Right";
-        choice3.innerText = "Wrong";
-        choice4.innerText = "Wrong";
-
-        choice2 = correctChoice;
-        choice1 = wrongChoice1;
-        choice3 = wrongChoice2;
-        choice4 = wrongChoice3;
+    choice1 = document.getElementById("choice0");
+    choice2 = document.getElementById("choice1");
+    choice3 = document.getElementById("choice2");
+    choice4 = document.getElementById("choice3");
 
 
+    correctOne = correctChoice.innerText;
 
+
+};
+
+
+let Begin = function(){
+
+    btn1.onclick = function(){correctClick(this)};
+    btn2.onclick = function(){correctClick(this)};
+    btn3.onclick = function(){correctClick(this)};
+    btn4.onclick = function(){correctClick(this)};
+
+  if(count === 0){
+      createQuestion("What is the square root of 4?", "Wrong", "Right", "Wrong", "Wrong", "Right");
+  }else if(count === 1){
+      createQuestion("What is 2-2?", "34", "4", "2", "0", "0");
+  }
+
+
+};
+
+correctClick = function(button){
+    if(button === correctOne){
+       numRight++;
     }
-
-
-
-};
-
-let inButton = function(){
-
-  createQuestion();
-
-  theFirstquestion();
-
-
-};
-
-let theFirstquestion = function(){
-
-
-
-    if(choice2.onclick){
-        theSecondquestion();
-    }else if(choice1.onclick){
-        theWrongNumber.innerText = "That is wrong!";
-    }else if(choice3.onclick){
-        theWrongNumber.innerText = "That is wrong!";
-    }else if(choice4.innerText){
-        theWrongNumber.innerText = "That is wrong!";
-    }
-
-};
-
-let theSecondquestion = function(){
-
-    theWrongNumber.innerText = " ";
-
-    questionText.innerText = "Reddit";
-
-    choice1.innerText = "Br";
-    choice2.innerText = "uh";
-    choice3.innerText = "Mo";
-    choice4.innerText = "ment";
-
+   count++;
+    Begin();
 };
