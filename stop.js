48156f1e09c47f7572d15815096c7a6428da0ca4
winicmic <53783136+winicmic@users.noreply.github.com>
2020-03-22 16:26:51 -0400

    Update stop.js

--- stop.js
+++ stop.js
@@ -12,15 +12,20 @@ btn4 = document.getElementById("btn3");
 
 let createQuestion = function(questionText, choice1, choice2, choice3, choice4, correctChoice){
 
-  questionText = document.getElementById("question");
+  let Thequestion = document.getElementById("question");
+  Thequestion.innerText = questionText;
 
-    choice1 = document.getElementById("choice0");
-    choice2 = document.getElementById("choice1");
-    choice3 = document.getElementById("choice2");
-    choice4 = document.getElementById("choice3");
+    let answer1 = document.getElementById("choice0");
+    answer1.innerText = choice1;
+    let answer2 = document.getElementById("choice1");
+    answer2.innerText = choice2;
+    let answer3 = document.getElementById("choice2");
+    answer3.innerText = choice3;
+    let answer4 = document.getElementById("choice3");
+    answer4.innerText = choice4;
 
 
-    correctOne = correctChoice.innerText;
+    correctOne = correctChoice;
 
 
 };
@@ -37,13 +42,26 @@ let Begin = function(){
       createQuestion("What is the square root of 4?", "Wrong", "Right", "Wrong", "Wrong", "Right");
   }else if(count === 1){
       createQuestion("What is 2-2?", "34", "4", "2", "0", "0");
+  }else if(count === 2){
+    createQuestion("What letter in the alphabet comes after 'H'?", "I", "Q", "A", "D", "I");
+  }else if(count === 3){
+    createQuestion("What is the best game?", "Red Dead Redemtion 2", "Super Mario Galaxy 2", "Lego Star Wars: The Complete Saga", "Dragon Ball Z: Kakarot", "Lego Star Wars: The Complete Saga");
+  }else{
+    let question = document.getElementById("question");
+    question.innerText = "You got " + numRight + " correct out of " + numQuestions;
+    document.getElementById("btn0").innerHTML = " ";
+    document.getElementById("btn1").innerHTML = " ";
+    document.getElementById("btn2").innerHTML = " ";
+    document.getElementById("btn3").innerHTML = " ";
+    document.getElementById("progress").innerHTML = " ";
   }
 
+document.getElementById("progress").innerText = "Question " + (count + 1) + "  of  " + numQuestions;
 
 };
 
 correctClick = function(button){
-    if(button === correctOne){
+    if(button.innerText === correctOne){
        numRight++;
     }
    count++;
